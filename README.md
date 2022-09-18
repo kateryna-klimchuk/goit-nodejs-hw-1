e8cfcf578b84ce29e055e6cabc1c1b663421af8b
kateryna klimchuk <ketyclima@gmail.com>
2022-09-18 21:37:50 +0300

    Added all files for home-work 1

---
+++ README.md
@@ -0,0 +1,17 @@
+Screenshots:
+
+# node index.js --action list
+
+https://monosnap.com/file/0ztqHRhQGsKaxTJcViwnZwWhoGtQuW
+
+# node index.js --action get --id 5
+
+https://monosnap.com/file/JvCkkz1T8Ix9IWvPDRvKhTd9yLQX9S
+
+# node index.js --action add --name Mango --email mango@gmail.com --phone 322-22-22
+
+https://monosnap.com/file/FMfglVfcskgVDSM8wALbaYs2OLCf2E
+
+# node index.js --action remove --id=3
+
+https://monosnap.com/file/o1IqRkclq4mcVfeD8ju6FtfY1jRXqi
---
+++ index.js
@@ -0,0 +1,36 @@
+const contacts = require("./contacts");
+
+const yargs = require("yargs");
+const { hideBin } = require("yargs/helpers");
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const allContacts = await contacts.listContacts();
+      console.table(allContacts);
+      break;
+
+    case "get":
+      const contact = await contacts.getContactById(id);
+      console.log(contact);
+      break;
+
+    case "add":
+      const newContact = await contacts.addContact(name, email, phone);
+      console.log(newContact);
+      break;
+
+    case "remove":
+      const removeContact = await contacts.removeContact(id);
+      console.log(removeContact);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+const arr = hideBin(process.argv);
+const { argv } = yargs(arr);
+
+invokeAction(argv);
